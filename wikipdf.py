9dc1d9d15b7816866d6f86612c6771bfbf043eae
thewonelolf <thewonelolf@github.com>
2019-02-28 15:48:32 -0500

    fixed encoding? formatting still trash.

--- wikipdf.py
+++ wikipdf.py
@@ -1,20 +1,23 @@
 from fpdf import FPDF
+import sys
+
 
 def pdfCreate(title, steps, url):
     pdf = FPDF()
+    pdf.add_font('arial','','arial.ttf',uni=True)
     pdf.add_page()
-    pdf.set_font("times", size=12)
-    pdf.cell(200, 10, txt=title, ln=1, align="C")
+    pdf.set_font("arial", size=12)
+    pdf.cell(10, 10, txt=title, ln=2, align="C")
 
     print(url)    
     x=0
     stepCount = len(steps)
     while x<stepCount-1:
-        pdf.set_font("times", size=18)
-        pdf.cell(200, 10, txt=str(x)+"."+"\n"+steps[x]["step"], ln=1, align="C")
+        pdf.set_font("arial", size=18)
+        pdf.cell(10, 10, txt=str(x+1)+". "+steps[x]["step"], ln=2, align="L")
         #print(steps[x]["pic"])
-        pdf.set_font("times", size=12)
-        pdf.cell(200, 10, txt=steps[x]["detailed"], ln=1, align="C")
+        pdf.set_font("arial", size=12)
+        pdf.cell(10, 10, txt=steps[x]["detailed"], ln=2, align="L")
         x+=1
 
     pdf.output("simple_demo.pdf")
