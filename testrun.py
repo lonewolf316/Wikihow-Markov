70b5ed773b6882a6b60fec27b58a36bf94172fc9
thelonewolf316 <thelonewolf316>
2019-03-01 16:20:12 -0500

    Removed testing file

--- testrun.py
+++
@@ -1,9 +0,0 @@
-from wikipdf import pdfCreate
-from pywikihow import WikiHow
-
-randomWiki = WikiHow.random()
-title = randomWiki["title"]
-steps = randomWiki["steps"]
-url = randomWiki["url"]
-
-pdfCreate(title, steps)
